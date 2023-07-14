2240b662870c61f89a2ae598622a8d19c101fe82
Warun S <warunsinx@gmail.com>
2023-07-14 17:15:36 +0700

    setup validation pipe with class vaildator, transformer

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,20 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsAlpha, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 @InputType()
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsEmail()
   @Field()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
   @Field()
   firstname: string;
+
+  @IsNotEmpty()
+  @IsAlpha()
   @Field()
   lastname: string;
 }
